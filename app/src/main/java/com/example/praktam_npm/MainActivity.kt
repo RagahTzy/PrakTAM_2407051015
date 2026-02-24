5c6e8b8ee0e701aa9466a9bf1664adbd81c21ca9
RagahTzy <ragahediting@gmail.com>
2026-02-24 09:24:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktam_npm/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.praktam_npm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktam_npm.ui.theme.PrakTAM_NPMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrakTAM_NPMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Halo, saya Ragah Mujahidin dengan NPM 2407051015 siap belajar Compose!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrakTAM_NPMTheme {
+        Greeting("Android")
+    }
+}
